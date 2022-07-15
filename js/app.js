451428f598b7686cd68556da634f358e318933c6
Sophia G20 Grzeskowiak-Amezquita <SophiaG20@SophiaG20.com>
2022-07-14 22:36:09 -0700

    Almost done

--- js/app.js
+++ js/app.js
@@ -1,69 +1,39 @@
-'use strict';
-
-let siteVistitor = prompt('What do you go by?');
--alert(`Hello ${siteVistitor}! Guess what I'm here for!`);
-+alert(`Hello ${siteVistitor}! Use the keyboard to guess what I'm here for!`);
-
-let score = 0;
-- // Here goessssss
-+ // Here goes the game
-
--let questionOne = prompt('Is G20 a common name?' (yes/no)').toLowerCase();
--if (questionOne === 'yes|| question1 === 'y') {
--    alert('Incorrect!');
-- score++;
-// console.log(You are Incorrect);
--} else {question1 === 'no|| question1 === 'n'} {
--  alert('Correct!');
-
-} else {
-- alert('Anser yes/no or y/n');
--// console.log('Answer yes/no or y/n');
--}  // end of questionOne
-
-let questionTwo = prompt('Would you try to say Grzeskowiak-Amezquita as fast as you can?').toLowerCase();
-if (questionTwo === 'yes|| question2 === 'y') {
-    alert('Correct!');
-    score++;
-// console.log(You are Correct);
-} else {question2 === 'no|| question2 === 'n'} {
-    alert('Incorrect!');
-// console.log(You are Incorrect);
-} else {
-    alert('Anser yes/no or y/n');
-    // console.log('Answer yes/no or y/n');
-} // end of questionTwo
-
-let questionThree = prompt('Am I Veteran of the US Army?').toLowerCase();
-if (questionThree === 'yes|| question === 'y') {
-    alert('Correct!');
-    score++;
-// console.log(You are Correct);
-} else if(questionThree === 'no|| question === 'n') {
-    alert('Incorrect!');
-// console.log(You are Incorrect);
-} else {
-    alert('Anser yes/no or y/n');
-    // console.log('Answer yes/no or y/n');
-} // end of questionThree
-
--let questionFour = prompt('Am I SGT in the US Army? (yes/no or y/n').toLowerCase();
--if (questionFour === 'yes|| question4 === 'y') {
--    alert('Incorrect!');
-- score++;  
-// console.log(You are Incorrect);
--} else {questionFour === 'no|| question4 === 'n'} {
--  alert('Correct!');
-// console.log(You are Correct);
-} else {
-    alert('Anser yes/no or y/n');
-+function yesAndnoQuestion(userAnswer){
-+   if (userAnswer === 'yes' || userAnswer === 'y') {
-+       return true;
-+   } else if (userAnswer === 'no' || userAnswer === 'n') {
-+       return false;
-+   } else {
-+       return 'Answer yes/no or y/n';
-    // console.log('Answer yes/no or y/n');
-+   }
+"use strict";
+let userName = prompt("What do you want to call yourself?");
+let vg = prompt("Does " + userName + " Have a common name? (yes/no)(y/n)");
+let vgAnswer = vg.toLowerCase();
+if (vgAnswer === "yes" || vgAnswer === "y") {
+  alert("You have a common name! unlike Sophia G20");
 }
+else {
+  alert("You do not have a common name like me!");
+}  // end of if statement
+let sleep = prompt("Does Sophia love sleep?");
+let sleepAnswer = sleep.toLowerCase();
+if (sleepAnswer === "yes" || sleepAnswer === "y") {
+    alert("Sophia loves sleep!");
+}   // end of if statement
+else {
+    alert("Unlike Sophia, You do not like sleep !");
+}  // end of else statement
+let hobbies = prompt("What are your hobbies?");
+let hobbiesArray = hobbies.split(",");
+let hobbiesArrayLength = hobbiesArray.length;
+let hobbiesArrayLengthString = hobbiesArrayLength.toString();
+alert("You have " + hobbiesArrayLengthString + " hobbies!");
+
+if ( vg !== "yes" && vg !== "y") {
+  alert("You are not a videogame fan!");
+  //console.log("You are not a videogame fan!");
+  window.alert("You are not a videogame fan!");
+} // end of if statement
+
+if (vgAnswer === "yes" || vgAnswer === "y") {
+  displayUserName();
+} // end of if statement
+
+questionaire();
+
+function displayUserName() {
+  alert("Bye " + userName + "!");
+} // end of displayUserName function
